fd04a905fc73410aebf7a8b0585b342e124f2532
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-09-29 15:28:13 -0400

    refactored

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -10,38 +11,31 @@ public class GameController : MonoBehaviour
     public TMP_Text LivesLabel;
     public TMP_Text ScoreLabel;
 
+    private float livesLabelHalfWidth;
+    private float livesLabelHalfHeight;
+
+    private float scoreLabelHalfWidth;
+    private float scoreLabelHalfHeight;
+
+    public CanvasScaler scaler;
+    public Vector2 scale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var currentResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        scale = currentResolution / scaler.referenceResolution;
+
+        livesLabelHalfWidth = LivesLabel.rectTransform.rect.width * 0.5f * scale.x;
+        livesLabelHalfHeight = LivesLabel.rectTransform.rect.height * 0.5f * scale.y;
+        scoreLabelHalfWidth = ScoreLabel.rectTransform.rect.width * 0.5f * scale.x;
+        scoreLabelHalfHeight = ScoreLabel.rectTransform.rect.height * 0.5f * scale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (Input.deviceOrientation)
-        {
-            case DeviceOrientation.Portrait:
-                //SceneLabel.text = "Portrait";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -200.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-353.0f, -200.0f);
-                break;
-            case DeviceOrientation.LandscapeLeft:
-                //SceneLabel.text = "LandscapeLeft";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -83.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-353.0f, -83.0f);
-                break;
-            case DeviceOrientation.LandscapeRight:
-                //SceneLabel.text = "LandscapeRight";
-                LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -83.0f);
-                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-500.0f, -83.0f);
-                break;
-            case DeviceOrientation.Unknown:
-                //SceneLabel.text = "Unknown";
-                break;
-            default:
-               // SceneLabel.text = "Some other orientation";
-                break;
-        }
+        LivesLabel.rectTransform.position = new Vector2(Screen.safeArea.xMin + livesLabelHalfWidth, Screen.safeArea.yMax - livesLabelHalfHeight);
+        ScoreLabel.rectTransform.position = new Vector2(Screen.safeArea.xMax - scoreLabelHalfWidth, Screen.safeArea.yMax - livesLabelHalfHeight);
     }
 }
